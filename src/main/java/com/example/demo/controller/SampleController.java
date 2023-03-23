c9c72b99bd727f01eeba3db3c7e2768fdc90c8d4
Andrey Nakin <andrey.nakin@gmail.com>
2023-03-23 13:48:24 +0300

    work

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.SampleEntity;
 import com.example.demo.repository.SampleRepository;
+import jakarta.annotation.PostConstruct;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,27 @@ public class SampleController {
     @GetMapping("/samples")
     @Transactional(readOnly = true)
     public List<SampleEntity> list() {
-        return repository.findAll();
+        return repository.findAllByOrderByName().toList();
+    }
+
+    @GetMapping("/samples/first")
+    @Transactional(readOnly = true)
+    public SampleEntity first() {
+        return repository.findAllByOrderByName().findFirst().orElseThrow();
+    }
+
+    @PostConstruct
+    @Transactional
+    void init() {
+        var e = new SampleEntity();
+        e.setId(UUID.randomUUID().toString());
+        e.setName("a");
+        e.setVal("b");
+        repository.save(e);
+
+        e.setId(UUID.randomUUID().toString());
+        e.setName("c");
+        e.setVal("d");
+        repository.save(e);
     }
 }
--- src/main/java/com/example/demo/repository/SampleRepository.java
+++ src/main/java/com/example/demo/repository/SampleRepository.java
@@ -1,6 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.SampleEntity;
+import java.util.stream.Stream;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SampleRepository extends JpaRepository<SampleEntity, Integer> {}
+public interface SampleRepository extends JpaRepository<SampleEntity, Integer> {
+
+    Stream<SampleEntity> findAllByOrderByName();
+}
